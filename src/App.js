6a5c34a6cfb3831838ec8904f13ec2adfdfdcf6f
Faizan Gaima <faizan@Faizans-MacBook-Pro.local>
2018-02-06 01:22:12 +0530

    Learnt Props and State

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons : [
+      {name:'John', age:26 },
+      {name:'Jane', age:27 },
+      {name:'Johnny', age:28 }
+    ]
+  }
+
+  switchNameHandler = (newName) => {
+    this.setState({
+      persons : [
+        {name: newName, age:26 },
+        {name:'Jane', age:27 },
+        {name:'Johnny', age:29 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
           <h1>Hi, I am learning react</h1>
+          <button onClick={ () => this.switchNameHandler( 'Faizan')}>Switch Name</button>
+          <Person name = { this.state.persons[0].name } age =  { this.state.persons[0].age }/>
+          <Person click = { this.switchNameHandler.bind(this, 'Faiz') } name =  { this.state.persons[1].name } age =  { this.state.persons[1].age }/>
+          <Person name =  { this.state.persons[2].name } age =  { this.state.persons[2].age }>
+             My Hobbies : Racing 
+          </Person>
       </div>
     );
   }
